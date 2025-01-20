254129af6d383ec46a8b876325a67aba03637545
srideep-banerjee <banerjee.srideep@gmail.com>
2025-01-20 13:31:26 +0530

    Create string_searcher.go
    Added StringSearcher to commands

---
+++ commands/string_searcher.go
@@ -0,0 +1,45 @@
+package commands
+
+type StringSearcher[T any] struct {
+    start *node[T]
+}
+
+type node[T any] struct {
+    nextEntries map[rune]*node[T]
+    values []T
+}
+
+func newNode[T any]() *node[T] {
+    n := new(node[T])
+    n.nextEntries = make(map[rune]*node[T])
+    n.values = make([]T, 0)
+    return n
+}
+
+func NewStringSearcher[T any]() *StringSearcher[T] {
+    ss := new(StringSearcher[T])
+    ss.start = newNode[T]()
+    return ss
+}
+
+func (s StringSearcher[T]) AddEntry(str string, value T) {
+    var n *node[T] = s.start
+    for _, ch := range str {
+        n.values = append(n.values, value)
+        if n.nextEntries[ch] == nil {
+            n.nextEntries[ch] = newNode[T]()
+        }
+        n = n.nextEntries[ch]
+    }
+}
+
+func (s StringSearcher[T]) GetAvailableValues(str string) []T {
+    var n *node[T] = s.start
+    for _, ch := range str {
+        if n.nextEntries[ch] == nil {
+            return []T{}
+        }
+        n = n.nextEntries[ch]
+    }
+    return n.values
+}
