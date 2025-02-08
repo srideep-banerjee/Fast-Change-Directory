1bead5e2e5d678a7d66598bf9fa7c95e75473d3f
srideep-banerjee <banerjee.srideep@gmail.com>
2025-02-08 23:38:07 +0530

    Removed Matches from Command interface and replaced Match with StringSearcher

--- commands/command.go
+++ commands/command.go
@@ -1,21 +1,26 @@
 package commands
 
+import (
+	"strings"
+)
+
 type Command interface {
-    Matches(str string) bool
     Validate(str string) (reason string)
     Process(str string) error
 }
 
-var commandList = []Command {
-    add {},
-    remove {},
+var availbleCommands map[string]Command = map[string]Command {
+	"add": add{},
+	"remove": remove{},
 }
 
+var commandSearcher *StringSearcher[Command] = NewStringSearcherWith(availbleCommands)
+
 func MatchesAny(str string) bool {
-    for _, cmd := range commandList {
-        if cmd.Matches(str) {
-            return true
-        }
+	str = str[1:]
+	spaceInd := strings.Index(str, " ")
+	if spaceInd != -1 {
+		str = str[:spaceInd]
     }
-    return false
+	return len(commandSearcher.GetAvailableValues(str)) != 0
 }
